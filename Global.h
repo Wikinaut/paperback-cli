9e96b69bffc5a1e9827704b8a038e7ff3f28e5b8
suhrke <suhrke@teknik.io>
2017-07-26 13:50:18 -0700

    Added portions of paperbak.h to Global.h

--- Global.h
+++ Global.h
@@ -6,7 +6,7 @@
  *    Description:  As paperback was designed around global memory management, some 
  *                  must remain global until serious refactoring
  *
- *        Version:  1.0
+ *        Version:  1.2
  *        Created:  07/25/2017 09:23:02 AM
  *       Revision:  none
  *       Compiler:  gcc
@@ -19,6 +19,7 @@
 
 #include <ctime>
 
+
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////// GENERAL DEFINITIONS //////////////////////////////
 typedef unsigned char  uchar;
@@ -26,6 +27,7 @@ typedef unsigned short ushort;
 typedef unsigned int   uint;
 typedef unsigned long  ulong;
 
+#define TEXTLEN        256             // Maximal length of strings
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,13 +49,14 @@ typedef struct t_data {                // Block on paper
   ushort         crc;                  // Cyclic redundancy of addr and data
   uchar          ecc[32];              // Reed-Solomon's error correction code
 } t_data;
-static_assert(sizeof(t_data)==128, sizeof(t_data));
+//static_assert(sizeof(t_data)==128);
 
 #define PBM_COMPRESSED 0x01            // Paper backup is compressed
 #define PBM_ENCRYPTED  0x02            // Paper backup is encrypted
 
-// FILETIME is 64-bit data type - to keep alignment, padding required if 
-// time_t is 32-bit signed integer (typically 64-bit)
+// FILETIME is 64-bit data type, time_t typically 64-bit, but was 32-bit in
+// older *NIX versions.  long and pointers are 64-bit in *NIX while only
+// 32-bit in Windows.  Depending on implementation, alignment issues possible
 typedef struct t_superdata {           // Identification block on paper
   ulong          addr;                 // Expecting SUPERBLOCK
   ulong          datasize;             // Size of (compressed) data
@@ -62,14 +65,17 @@ typedef struct t_superdata {           // Identification block on paper
   uchar          mode;                 // Special mode bits, set of PBM_xxx
   uchar          attributes;           // Basic file attributes
   ushort         page;                 // Actual page (1-based)
-  //FILETIME       modified;             // Time of last file modification
+#ifdef _WIN32
+  FILETIME       modified;             // Time of last file modification
+#elif __linux__
   time_t         modified;
+#endif
   ushort         filecrc;              // CRC of compressed decrypted file
   char           name[64];             // File name - may have all 64 chars
   ushort         crc;                  // Cyclic redundancy of previous fields
   uchar          ecc[32];              // Reed-Solomon's error correction code
 } t_superdata;
-static_assert(sizeof(t_superdata)==sizeof(t_data));
+//static_assert(sizeof(t_superdata)==sizeof(t_data));
 
 typedef struct t_block {               // Block in memory
   ulong          addr;                 // Offset of the block
@@ -84,8 +90,11 @@ typedef struct t_superblock {          // Identification block in memory
   ulong          origsize;             // Size of original (uncompressed) data
   ulong          mode;                 // Special mode bits, set of PBM_xxx
   ushort         page;                 // Actual page (1-based)
-  //FILETIME       modified;             // Time of last file modification
+#ifdef _WIN32
+  FILETIME       modified;             // Time of last file modification
+#elif __linux__
   time_t         modified;
+#endif
   ulong          attributes;           // Basic file attributes
   ulong          filecrc;              // 16-bit CRC of decrypted packed file
   char           name[64];             // File name - may have all 64 chars
